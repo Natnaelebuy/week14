e527d0dc51402fa6a98748a91abb19a3b46c1ad1
Natnael <natnaelebuy@gmail.com>
2022-05-09 20:46:04 -0700

    update summarey

--- demo/src/main/java/com/example/HeapAppExample.java
+++ demo/src/main/java/com/example/HeapAppExample.java
@@ -1,8 +1,10 @@
-package com.heap;
+package com.example;
 
 import java.util.*;
 
 public class HeapAppExample {
+
+  private static Integer[] arr;
 
   public static void main(String[] args) {
     // create a min heap
@@ -19,14 +21,30 @@ public class HeapAppExample {
       System.out.printf("%d ", val);
       // student needs to write code to
       // put the random value (val) into the heap
+      minHeap.add(val);
     }
     System.out.printf("\n");
     System.out.printf("\n");
     // student needs to write code to
     // print the heap, without changing the heap
+   Iterator<Integer> iterator = minHeap.iterator();
+   System.out.println("printing the heap");
+   while (iterator.hasNext()) {
+     System.out.print(iterator.next() + " ");
+   }
+  System.out.printf("\n");
+  System.out.printf("\n");
 
     // student needs to write code to
     // print the 5 smallest values in the heap
     // the values can be removed from the heap (the heap can be changed)
+    System.out.printf("printing the top 5 race time ");
+    for(int i=1; i <6; i++){
+      System.out.println("#" +i + " " + minHeap.poll());
+    }
+    
+
+    
+    
   }
 }
---
+++ demo/src/main/java/com/example/array.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class array {
+
+    public static int length;
+
+}
